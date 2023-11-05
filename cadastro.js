16d6cefbbf0943238dc4cd1605672d6db18ac2a3
Kote <144833957+YakinFsilva@users.noreply.github.com>
2023-11-05 09:35:29 -0300

    Add files via upload

---
+++ cadastro.js
@@ -0,0 +1,16 @@
+//Nome (string): Armazenará o nome do usuário.
+//Salário (number): Armazenará o salário.
+//Idade (number): Armazenará a idade.
+//Possui Diploma (boolean): Armazenará um valor booleano true ou false.
+
+var nome = "Robson"; 
+var salario = 3600; 
+var idade = 26; 
+var possuiDiploma = true; 
+
+//display
+
+console.log ("Nome: " + nome)
+console.log ("Salario: " + salario)
+console.log("Idade: " + idade)
+console.log ("Possui diploma: " + (possuiDiploma ? "sim" : "não"));
