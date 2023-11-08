33b261209df0938322e63c1a0d206081725f9d1b
Kote <144833957+YakinFsilva@users.noreply.github.com>
2023-11-08 15:39:13 -0300

    Add files via upload

---
+++ Calculadora-de-Media.js
@@ -0,0 +1,7 @@
+var nota1 = prompt("Insira a primeira nota (entre 0 e 10):");
+var nota2 = prompt("Insira a segunda nota (entre 0 e 10):");
+var nota3 = prompt("Insira a terceira nota (entre 0 e 10):");
+
+var media = (nota1 + nota2 + nota3) / 3;
+
+console.log("media:" + media)
